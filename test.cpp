805ed42756c4b3d7238429ef684a82d549de4306
Charlize Brodeth <ckbrodeth25@gmail.com>
2025-09-12 18:13:41 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout <<"Hello World, Im ready for opesy heheeee" << std::endl;
+    std::cout <<"Hello World, Im ready for STDISCM heheeee" << std::endl;
     return 0;
+
 }
